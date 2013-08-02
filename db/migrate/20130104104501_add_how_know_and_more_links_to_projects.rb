88aa31af0376f3b97c61eaf245872c4a3829f056
Renan Coutinho <rscoutinho@live.com>
2013-08-02 18:36:11 -0300

    first

--- db/migrate/20130104104501_add_how_know_and_more_links_to_projects.rb
+++ db/migrate/20130104104501_add_how_know_and_more_links_to_projects.rb
@@ -2,5 +2,6 @@ class AddHowKnowAndMoreLinksToProjects < ActiveRecord::Migration
   def change
     add_column :projects, :how_know, :text
     add_column :projects, :more_links, :text
+    add_column :projects, :budget1, :text
   end
 end
--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -67,7 +67,7 @@ describe "Projects" do
       [
         'permalink', 'name', 'video_url',
         'headline', 'goal', 'online_days',
-        'about', 'first_backers', 'how_know', 'more_links'
+        'about', 'first_backers', 'how_know', 'more_links','budget1'
       ].each do |a|
         fill_in "project_#{a}", with: project.attributes[a]
       end
