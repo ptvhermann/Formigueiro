e5b3dc102e1fac83015bc352f2708903d8a56db0
GabAlexandria <gabagrant@gmail.com>
2013-08-11 00:00:07 -0400

    remove weird line

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,7 +33,6 @@ namespace :assets do
 
     desc "Link assets for current deploy to the shared location"
     task :update do
-      run "mkdir -p #{release_path}/#{link}"
       shared_assets.each { |link| run "ln -nfs #{shared_path}/#{link} #{release_path}/#{link}" }
     end
   end
